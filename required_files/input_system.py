7fa0ad82c5ae0f3db8090de5a9eb8f6de223e5bb
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-04-15 01:24:06 +0100

    Input system done

--- required_files/input_system.py
+++ required_files/input_system.py
@@ -1,4 +1,12 @@
 print('\nPlease input the items you would like to calulate the required items for.\nTo stop type the word "End" into the input field and the program will start to calulate.')
 user_input = 'Null'
+run = 0
 while user_input != 'End':
-    
+    print(' ')
+    user_input = input('Please input the item here : ')
+    if user_input != 'End':
+        user_input_list[0] = user_input
+
+input_file = open('input_file.txt','w')
+input_file.write(user_input_list)
+input_file.close()
