f702d44d985a308a7c74261e5c1be7337b824200
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-04-15 17:22:05 +0100

    Code commented

--- dev_tools/database_compiler.py
+++ dev_tools/database_compiler.py
@@ -9,12 +9,14 @@ while done == 'No':
     line_on = line_on + 1
     current_line = unsorted_database[line_on]
     if current_line[0] == 'E' and current_line[1] == 'n' and current_line[2] == 't' and current_line[3] == 'r' and current_line[4] == 'y' and current_line[5] == '-':
+        # cheacks the first 6 caracters of the entry are correct
         start_of_entry = line_on
         end_of_entry_not_found = 'Yes'
         while end_of_entry_not_found == 'Yes':
             line_on = line_on + 1
             if unsorted_database[line_on] == 'End of entry':
                 end_of_entry = line_on
+                # marks the end of the entry
                 end_of_entry_not_found = 'No'
         run = 0
         for x in range(start_of_entry,end_of_entry):
@@ -24,6 +26,7 @@ while done == 'No':
         new_file = open(new_filename,'wt')
         new_file.write(entry_own_file)
         new_file.close()
+        # writes to its own file therefore sorting that bit of data
     if line_on == total_lines:
         done = 'Yes'
 print('\nThe databse has now been sorted!')
