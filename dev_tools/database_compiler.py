88b103628c906d464247d13051b9d085c3c81bd2
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-08-23 01:41:11 +0100

    bug fixes and opimisations for the code

--- dev_tools/database_compiler.py
+++ dev_tools/database_compiler.py
@@ -1,10 +1,11 @@
-file_to_sort = open('Unorganised_database.txt','r')
-unsorted_database = file_to_sort.readlines()
+file_to_sort = open('Unorganised_database.txt','rt')
+# opens the databse it needs to sort
+unsorted_database = file_to_sort.read().splitlines()
 # reads the contents of the databse it needs to sort
 file_to_sort.close()
 total_lines = len(unsorted_database)
 # calulates the number of the final index number of the unsorted databse
-final_entry = total_lines+1
+final_entry = total_lines - 1
 done = 'No'
 line_on = -1
 # sets varibles to start the loop
@@ -22,15 +23,20 @@ while done == 'No':
                 # marks the end of the entry
                 end_of_entry_not_found = 'No'
         run = 0
+        entry_own_file = list()
         for x in range(start_of_entry,end_of_entry):
-            entry_own_file[run] = unsorted_database[run+start_of_entry+1]
+            entry_own_file.append(unsorted_database[run+start_of_entry])
             run = run + 1
         new_filename = unsorted_database[start_of_entry]+'.txt'
         new_file = open(new_filename,'wt')
-        new_file.write(entry_own_file)
+        run = 0
+        for x in range(0,len(entry_own_file)):
+            new_file.write(entry_own_file[run])
+            new_file.write('\n')
+            run = run + 1
         new_file.close()
         # writes to its own file therefore sorting that bit of data
-    if line_on == total_lines:
+    if line_on == final_entry:
         done = 'Yes'
 print('\nThe databse has now been sorted!')
 input('Press enter to exit')
