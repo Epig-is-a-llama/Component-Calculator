529376076d39f0efad5ccbca93f514a9b44f6594
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-08-24 21:30:15 +0100

    code completed and bug tested

--- seperate_file_system.py
+++ seperate_file_system.py
@@ -1,47 +1,50 @@
 print('\nLoading...\n')
 
-# declares and sets global variables
-global output_list
-output_list = list()
-global database_name
-global user_input_list
-global sorted_output_list
-global database_settings
+# creates and makes global the list which will contain the raw output
+global raw_output
+raw_output = list()
 
-def searching_system(wanted_items,raw_output):
+def searching_system(wanted_items,database_name):
     types_of_needed_items_dobble = len(wanted_items)
     types_of_needed_items = types_of_needed_items_dobble/2
+    global requested_items
+    requested_items = wanted_items
+    print(requested_items)
     # as the items and number of those items are on seperated items we neeed to half the length of the `whanted_items` list
-    item_on = -2
+    item_on = 0
     # sets a variable the loop will need to run itself
     while types_of_needed_items >= item_on:
-        item_on = item_on+2
-        # goes up in twos as each entry takes two lines
-        current_item = whanted_items[item_on]
-        current_item_number = whanted_items[item_on + 1]
+        current_item = wanted_items[item_on]
+        current_item_number = int(wanted_items[item_on + 1])
         # gets the number of items and what items out of the whanted items list
-        print('Searching database for entry of : ',current_item)
+        print('\nSearching database for entry of : ',current_item)
         # gives the user info mation as to what the progarm is doing
         item_needed = current_item
-        current_item = 'C:\Slimefun-Calultator/premade_databases/',database_name,'/Entry-'+current_item+'.txt'
+        current_item = 'C:/Component-Caulator/premade_databases/'+database_name+'/Entry-'+current_item+'.txt'
         # makes a varible which will be what the file's name will be so the progarm can propery find it and read the entry
         for x in range(0,current_item_number):
             # loops the apporitate number of times to get the correct number of resorses
             try:
-                data_file = open(filename,'rt')
+                data_file = open(current_item,'rt')
                 # trys to open a file as if it fails that means the progarm has reached the end of what it can calulate in that chain
             except Exception as e:
-                # goes to the function that writes the raw materials
+                # writes the raw materials
                 raw_output.append(item_needed)
                 # makes sure its always entered to a seperate location in the list
                 print('\nAdded the following entry to the output file: ',item_needed)
             else:
-                data_file = open(filename,'rt')
+                data_file = open(current_item,'rt')
                 # if an entry can be found however it then reads it and sees what it needed to compleate that entry and will repeat the process until it gets to just the raw materials
-                entry_data = data_file.read().splitlines()
+                raw_entry_data = data_file.read().splitlines()
+                runs = 0
+                entry_data = list()
+                for x in range(0, len(raw_entry_data)):
+                    entry_data.append(raw_entry_data[runs])
+                    runs = runs + 1
                 data_file.close()
-                raw_output.append(searching_system(entry_data,raw_output))
-        return raw_output
+                (searching_system(entry_data,database_name))
+        item_on = item_on+2
+        # goes up in twos as each entry takes two lines
 
 def database_selection_system():
     print('\nPlease input the name of the database you wish to use.')
@@ -50,7 +53,7 @@ def database_selection_system():
     return database_name
 
 def database_settings_system(database_name):
-    settings_file_location = 'C:\Slimefun-calculator/premade_databases/'+database_name+'/database_settings_file.txt'
+    settings_file_location = 'C:/Component-Caulator/premade_databases/'+database_name+'/database_settings_file.txt'
     database_settings_file = open(settings_file_location,'rt')
     database_settings = database_settings_file.read().splitlines()
     # reads the database settings file which can be used to configure the size of stacks and even to run that system the itention is to have it able to manilulate the progarm from this file
@@ -77,42 +80,54 @@ def input_system(database_name):
     # returns the requested items
     return user_input_list
 
-def output_system(raw_output):
+def output_system(raw_output,database_settings):
+    global output_sort_runs
+    global unsorted_output
+    global sorted_output_list
+    global matching_runs
+    global unsorted_output_len
+    global sorted_output_list_len
+    global line_on
     print('\nYour calulations are now done the progarm will now convert the output into a readable form\n')
-    unsorted_output = output_list
+    unsorted_output = raw_output
     # prepares to sort the unsorted output which is in an almost impossible to read format
     unsorted_output_len = len(unsorted_output)
     # by doing this command outside of the loop it doesn't have to run it every time the loop runs therefore speeding up the progarm
-    not_done == 'Yes'
+    not_done = 'Yes'
     output_sort_runs = 0
+    sorted_output_list = list()
     # sets these variables to start the loop
     while not_done == 'Yes':
         sorted_output_list_len = len(sorted_output_list)
-        sorted_output_list_final_num = sorted_output_list_len - 1
+        sorted_output_list_final_num = sorted_output_list_len
         matching_runs = 0
+        no_match_found = 'Yes'
         while no_match_found == 'Yes':
-            if unsorted_output[output_sort_runs] == sorted_output_list[matching_runs]:
+            if matching_runs == sorted_output_list_final_num:
+                # however if the progarm is unable to find a maching entry it will create a new entry
+                sorted_output_list.append(unsorted_output[output_sort_runs])
+                sorted_output_list.append('1')
+                no_match_found = 'No'
+            elif unsorted_output[output_sort_runs] == sorted_output_list[matching_runs]:
                 # if the progarm finds an existing entry with this name it will add 1 to it and move on
                 number_of_existing_entrys = sorted_output_list[matching_runs+1]
                 number_of_existing_entrys = int(number_of_existing_entrys)
                 new_number_of_entrys = number_of_existing_entrys + 1
                 sorted_output_list[matching_runs+1] = str(new_number_of_entrys)
                 no_match_found = 'No'
-            elif matching_runs == sorted_output_list_final_num:
-                # however if the progarm is unable to find a maching entry it will create a new entry
-                sorted_output_list.append(unsorted_output[output_sort_runs])
-                sorted_output_list[sorted_output_list_len + 1] = '1'
             matching_runs = matching_runs + 1
-        if output_sort_runs == unsorted_output_len:
-            not_done = 'No'
             # when its done going thogth the unsorted list it will automaticly brake the loop
         output_sort_runs = output_sort_runs + 1
+        if output_sort_runs == unsorted_output_len:
+            not_done = 'No'
     if database_settings[1] == 'Yes':
         # will only run this system if its enabled in the database settings and will run it according to how those settings dictate
         not_done = 'Yes'
         line_on = 1
-        stack_size = database_settings_file[3]
+        stack_size = database_settings[3]
         stack_size_int = int(stack_size)
+        sorted_output_list_len = len(sorted_output_list)
+        stack_system_final_line = sorted_output_list_len + 1
         # sets these varibles for the loop which is on the next line
         while not_done == 'Yes':
             current_number = sorted_output_list[line_on]
@@ -123,20 +138,24 @@ def output_system(raw_output):
                 new_number = new_number - stack_size_int
                 # until the number is below the set stack size it will keep calulating to see how many stacks of each item are needed
                 stack_count = stack_count + 1
+            current_number_str = str(current_number)
             stack_count_str = str(stack_count)
             new_number_str = str(new_number)
             # formats the output to include the newley caulualated values
             if new_number > 0:
-                new_line_contents = current_number+'       ( '+stack_count_str+' stacks and '+new_number+' items ) '
+                new_line_contents = current_number_str+'       ( '+stack_count_str+' stacks and '+new_number_str+' items ) '
             else:
-                new_line_contents = current_number+'       ( '+stack_count_str+' stacks ) '
+                new_line_contents = current_number_str+'       ( '+stack_count_str+' stacks ) '
             sorted_output_list[line_on] = new_line_contents
             line_on = line_on + 2
+            if line_on == stack_system_final_line:
+                not_done = 'No'
     # adds the end result to a .txt file and then prints the output to screen
+    formated_output = "\n".join(sorted_output_list)
     sorted_output_file = open('sorted_output_file.txt','wt')
-    sorted_output_file.write(sorted_output_list)
+    sorted_output_file.write(formated_output)
     sorted_output_file.close()
-    print('\nThis is your final output:\n',sorted_output_list)
+    print('\nThis is your final output:\n',formated_output)
     # the input is so the progarm doesn't just close when its done
     input('Press enter to close this window and open the output in a .txt file')
     exit()
@@ -151,7 +170,7 @@ settings = database_settings_system(database)
 requested_items = input_system(database)
 # tells the user their calulations will begin shortly
 print('\nYour calulations will begin shortly.\n')
-# creates the list which will contain the raw output
-unsorted_output = list()
 # runs the function to start working out the needed items
-unsorted_output = searching_system(requested_items,unsorted_output)
+searching_system(requested_items,database)
+# calls the function to give the user their results
+output_system(raw_output,settings)
