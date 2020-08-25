5b2d092104c9b4951809c45c4064e643757ad6e0
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-08-26 00:58:05 +0100

    Update installer.bat
    Installer polished and refined removing uneeded commands and installation parts as well as better instuctions.

--- installer_files/installer.bat
+++ installer_files/installer.bat
@@ -1,15 +1,10 @@
 ECHO off
-title Showing Instructions
-ECHO When installing this program please ensure that python it added to path. As will be shown in the screen shot displayed after you close this message.
+title Component Calculator Installer
+ECHO If python is already installed to this machine when the python installer shows please close it however if it is not yet installed please install it.
 pause
-title Showing Demo Screen Shot
-@install_demo.png
-timeout 30
 title Waiting For Python to be Installed
 @python-3.8.1-amd64-webinstall.exe
-title Installing pyshortcuts
-pip install pyshortcuts
 title Extracting Code
 @code_extractor.py
-tile Launching Program
-@c:\Slimefun-Calculator\launch.bat
+ECHO Done!
+pause
