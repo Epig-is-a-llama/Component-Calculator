2e828bc5ec1ea163a6d07f92b825352ecf47bfbf
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-08-25 14:25:45 +0100

    added config creation system and commented it and some other parts of the code where commenting was sparse

--- dev_tools/database_compiler.py
+++ dev_tools/database_compiler.py
@@ -1,3 +1,6 @@
+import os
+# imports the os module for
+
 file_to_sort = open('Unorganised_database.txt','rt')
 # opens the databse it needs to sort
 unsorted_database = file_to_sort.read().splitlines()
@@ -31,6 +34,7 @@ while done == 'No':
         new_file = open(new_filename,'wt')
         run = 1
         for x in range(0,(len(entry_own_file)) - 1):
+            # writes the entry into the file
             new_file.write(entry_own_file[run])
             new_file.write('\n')
             run = run + 1
@@ -38,5 +42,30 @@ while done == 'No':
         # writes to its own file therefore sorting that bit of data
     if line_on == final_entry:
         done = 'Yes'
-print('\nThe databse has now been sorted!')
-input('Press enter to exit')
+print('\nThe databse has now been sorted now the database configuration file must be setup.')
+config_template = open('config_template.txt','rt')
+# opens reads into a list then closes the config template file
+config_template_contents = config_template.read().splitlines()
+config_template.close()
+config_list = list()
+# sets vars in preperation for the loop
+runs = 0
+for x in range (0,(len(config_template) / 2)):
+    # loops once per config setting (as each setting takes two lines with one as the tile and one as the value)
+    print('\nFor the setting of: ',config_template_contents[runs],' what would you like to set for this setting?')
+    # asks the user what value they want to set each setting in the config to
+    setting = input('Please answer here:')
+    config_list.append(config_template_contents[runs])
+    # appends the setting title and then the setting value to the list which will be later writen to the new config file
+    config_list.append(config_template_contents[runs + 1])
+    # increases by two at a time as each setting takes two lines
+    runs = runs + 2
+runs = 0
+new_config_file = open('database_settings_file.txt','wt')
+for x in range (0,len(config_list)):
+    new_config_file.write(config_list[runs])
+    new_config_file.write('\n')
+    runs = runs + 1
+new_config_file.close()
+print('Config file created and saved.')
+input('Press enter to exit: ')
