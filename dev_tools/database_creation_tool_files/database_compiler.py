bcc4e0346baf192453a62ed7fb3351afc4660750
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-08-25 23:17:52 +0100

    small change to the database complier to tell the user were the output files are

--- dev_tools/database_creation_tool_files/database_compiler.py
+++ dev_tools/database_creation_tool_files/database_compiler.py
@@ -104,7 +104,7 @@ shutil.copyfile(original, target)
 # runs the command which starts the batchfile which contains the code to create the .exe file
 subprocess.call([r'C:/Component-Calculator/dev_tools/databse_packer.bat'])
 # deletes the temporary file which is used to house the database while it is being put into the database installer
-os.remove('database.zip') 
+os.remove('database.zip')
 # tells the user the .exe file has been made and tells them they may now close the program
 print('\n.EXE installer for the database created with the name set as: `'+database_name+' installer` when run this will automaticly install the database onto the computer if they have the base program already installed if it does not it will direct them to do so to use this database.\n)
-input('Now that the database has been fully processed you may now press enter to exit: ')
+input('Now that the database has been fully processed and saved to the compiled database folder you may now press enter to exit: ')
