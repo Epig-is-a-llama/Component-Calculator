7978541d60b90a4995f7d959678b40f4d0d769b9
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-08-27 18:02:00 +0100

    unneded like removed

--- dev_tools/database_creation_tool_files/database_extractor.py
+++ dev_tools/database_creation_tool_files/database_extractor.py
@@ -1,7 +1,6 @@
 from zipfile import ZipFile
 #create a zipfile object and load zip in it
 with ZipFile('database.zip', 'r') as zipObj:
-    zip.printdir()
     #extract all the contents of zip file into current directory
     zipObj.extractall(path = 'C:/Component-Calculator/databases/')
     zipObj.close()
