bd50f43ad48d4bcc3c0f6ad31cc19166d6393115
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-08-30 01:21:32 +0100

    as the prompt about python and the component calulator being needed is now included in a prompt opened by the .exe file using a prompt in the bach file isn't needed

--- dev_tools/database_creation_tool_files/database_package_installer.cmd
+++ dev_tools/database_creation_tool_files/database_package_installer.cmd
@@ -1,4 +1,2 @@
-echo off
-echo For this program to work python must be installed (download it at https://www.python.org/). In addition the Component-Calculator must be installed (download it at https://github.com/Epig-is-a-llama/Component-Calculator). If either of these are not installed please close this installer and run it once they have been installed if both are installed however please continue:
-pause
+ECHO OFF
 @database_extractor.py
