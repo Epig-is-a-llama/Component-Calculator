490f43f722b19cacfbbe5c30cab1a8a1b07135a9
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-08-30 01:20:32 +0100

    comments and messages edited to be more descriptive

--- dev_tools/database_creation_tool_files/database_extractor.py
+++ dev_tools/database_creation_tool_files/database_extractor.py
@@ -1,7 +1,7 @@
 from zipfile import ZipFile
 #create a zipfile object and load zip in it
 with ZipFile('database.zip', 'r') as zipObj:
-    #extract all the contents of zip file into current directory
+    #extract all the contents of zip file into specified directory
     zipObj.extractall(path = 'C:/Component-Calculator/databases/')
     zipObj.close()
-print('\nDone!\n')
+print('\nDone installing this database!\n')
