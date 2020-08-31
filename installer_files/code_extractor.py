d8f0c153c304a5baa36c48588e936f917377e82d
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-08-31 02:16:14 +0100

    removed shortcut creation system until a better solution can be found

--- installer_files/code_extractor.py
+++ installer_files/code_extractor.py
@@ -14,13 +14,4 @@ with ZipFile('files.zip', 'r') as zipObj:
     zipObj.extractall(path = 'C:/')
     zipObj.close()
 
-print('Would you like to create a desktop shortcut for the program? (Please answer in the form Y/N)')
-shortcut_ans = input('Please answer here: ')
-if shortcut_ans == 'Y':
-    original = r'C:/Component-Calculator/Component-Calultator.EXE'
-    desktop = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop')
-    target = desktop
-    # creates a copy of the shortcut for the .EXE verion of the launch.bat
-    shutil.copyfile(original, target)
-
 print('\nInstallation of the base program compleate!\nThe program will now open a window which contains all the installers databases which have been uploaded to the github page as well as the devloper tools pack for making databases once you are done with installing all of the modules you wish to or if you do not wish to install any close the folder and the installation of the program will be finalised.')
