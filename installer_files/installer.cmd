1c39cdc7e24a308b0c759e173fba7c125cf6640a
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-09-03 00:54:22 +0100

    Update installer.cmd
    Adds extra detail to the instuction regarding the module and database installers.

--- installer_files/installer.cmd
+++ installer_files/installer.cmd
@@ -4,7 +4,7 @@ title Component Calculator Installer
 set /p python_test=<python_installed_test_result.txt
 if %python_installed%==0 @python-3.8.1-amd64-webinstall.exe
 @code_extractor.py
-ECHO The installer will now open the folder containing all the installer files for different databases and modules.
+ECHO The installer will now open the folder containing all the installer files for different optional databases and modules. Please note that if this is the first version after a major update (for example 3.0.0) it is unlikely any databases are included as the update would have meant they need to be modified.
 pause
 explorer "C:\Component-Calultator\Module-Installers"
 ECHO The installation is now fully complete. The program is installed to 'C:\Component-Calculator'.
