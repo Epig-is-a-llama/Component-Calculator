238f6949b6a7b7012fe26f0cbe61b0abf1c200eb
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-09-03 01:40:29 +0100

    Update code_extractor.py
    Code extractor configured and finished off.

--- installer_files/code_extractor.py
+++ installer_files/code_extractor.py
@@ -1,17 +1,42 @@
 import shutil
 import os
 # both are imported for file manipulation
+
+from zipfile import ZipFile
+# imported for the unziping funcionality
+
+def unzip(zip_file , location_to_unzip_to):
+    with ZipFile(zip_file, 'r') as zipObj:
+        # create a zipfile object and load the zip into it
+        zipObj.extractall(path = location_to_unzip_to)
+        # extract all the contents of zip file into the specified directory
+        zipObj.close()
+
+def create_folder(directory):
+    try:
+        if not os.path.exists(directory):
+            # checks if the folder already exists if it does not it creates it
+            os.makedirs(directory)
+    except OSError:
+        pass
+
 try:
     shutil.rmtree('C:/Component-Calculator')
 except Exception as e:
     pass
 # old contents of the program folder are cleared which the user is warned of before the installation takes place
-from zipfile import ZipFile
-#create a zipfile object and load zip in it
-with ZipFile('files.zip', 'r') as zipObj:
-    zip.printdir()
-    #extract all the contents of zip file into the specified directory
-    zipObj.extractall(path = 'C:/')
-    zipObj.close()
+create_folder('C:/Component-Calculator')
+# creates the folder to install to
+
+create_folder('C:/Component-Calculator/Module-Installers')
+# creates the folder for the module installers to be unziped into
+unzip('Module-Installers.zip' , 'C:/Component-Calculator/Module-Installers')
+# unzips the module installers zip file into the correct location
+
+create_folder('C:/Component-Calculator/other-dependencys')
+# creates the folder for the other dependencys to be unziped to
+unzip(('Other-Files.zip' , 'C:/Component-Calculator/other-dependencys')
+# unzips the other dependencys into the correct folder
 
 shutil.copy('launch.cmd' , 'C:/Component-Calculator/launch.cmd')
+# creates a copy of the launch file in the base directory of the program
