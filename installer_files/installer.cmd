c0651e67fb5af195a81bd276d9191faa3e44e3a2
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-09-04 07:10:34 +0100

    Removed the finished message from the installer.cmd as its now shown as a popup by the .exe file.

--- installer_files/installer.cmd
+++ installer_files/installer.cmd
@@ -6,6 +6,4 @@ if %python_test%==0 @python-3.8.1-amd64-webinstall.exe
 @code_extractor.py
 ECHO The installer will now open the folder containing all the installer files for different optional databases and modules. Please note that if this is the first version after a major update (for example 3.0.0) it is unlikely any databases are included as the update would have meant they need to be modified.
 pause
-ECHO The installation is now fully complete. The program is installed to 'C:\Component-Calculator'.
 explorer "C:\Component-Calultator\Addon-Installers"
-pause
