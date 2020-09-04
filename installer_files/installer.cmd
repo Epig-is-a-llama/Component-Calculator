61dac0cc83b6b865df8f3c964486ad716aeca466
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-09-04 03:18:50 +0100

    Missing file extenstion on the python installation test file calling system added.

--- installer_files/installer.cmd
+++ installer_files/installer.cmd
@@ -1,6 +1,6 @@
 ECHO OFF
 title Component Calculator Installer
-@python_installed_test
+@python_installed_test.py
 set /p python_test=<python_installed_test_result.txt
 if %python_installed%==0 @python-3.8.1-amd64-webinstall.exe
 @code_extractor.py
