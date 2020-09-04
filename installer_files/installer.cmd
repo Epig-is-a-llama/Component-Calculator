699053b70dd1c9bb4ea732b59b39ed12ad092206
Epig-is-a-llama <60323417+Epig-is-a-llama@users.noreply.github.com>
2020-09-04 07:06:25 +0100

    Fix to the installer reading the incorrect var.

--- installer_files/installer.cmd
+++ installer_files/installer.cmd
@@ -2,7 +2,7 @@ ECHO OFF
 title Component Calculator Installer
 @python_installed_test.py
 set /p python_test=<python_installed_test_result.txt
-if %python_installed%==0 @python-3.8.1-amd64-webinstall.exe
+if %python_test%==0 @python-3.8.1-amd64-webinstall.exe
 @code_extractor.py
 ECHO The installer will now open the folder containing all the installer files for different optional databases and modules. Please note that if this is the first version after a major update (for example 3.0.0) it is unlikely any databases are included as the update would have meant they need to be modified.
 pause
